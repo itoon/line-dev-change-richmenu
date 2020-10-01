d762036b7b7fa70ea9300d6087a2a4b184b8b098
Songklod <itoon.sit@gmail.com>
2020-10-01 12:17:51 +0700

    init project

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+### Instrction
+1. install express, install package @line/bot-sdk, axios, cors, dotenv, nodemon --save-dev
+2. set CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET, dotenv
+3. GET, POST
+4. line config
+5. play with richMenu
---
+++ index.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('btn');
+const liffId = '';
+let userId = '';
+
+function main(){
+    // 1.liff init
+    // 2.liff ready get userid
+}
+
+main()
+
+function send(e){
+    e.preventDefault();
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;    
+    // 3. axios post data
+}
